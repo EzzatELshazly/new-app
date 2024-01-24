85f4e2d1721fc94aa0e31667bc6dd11a775df903
EzzatELshazly <65233473+EzzatELshazly@users.noreply.github.com>
2024-01-24 10:37:38 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,30 +2,40 @@ pipeline {
     agent any
 
     environment {
+        
         DOCKER_REGISTRY = "ezzatelshazly"
         DOCKER_IMAGE = "firstpipeline"
         imageTagApp = "build-${BUILD_NUMBER}-app"
         imageNameapp = "${DOCKER_REGISTRY}:${imageTagApp}"
         OPENSHIFT_PROJECT = 'ezzatelshazly'
-
-    }
+        GITHUB_REPO = "EzzatELshazly/new-app"
+        OPENSHIFT_SERVER = 'https://api.ocpuat.devopsconsulting.org:6443'
+        APP_SERVICE_NAME = 'servicePipelineEE'
+        APP_PORT = '8080'
+        APP_HOST_NAME = 'servicePipelineEE.apps.ocpuat.devopsconsulting.org'
+        }
 
     stages {
+        
+        
+        stage('Checkout') {
+            steps {
+                git url: "https://github.com/${GITHUB_REPO}.git", branch: 'main'
+            }
+        }
+        
         stage('Build Docker image for app.py and push it to docker hub') {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_REGISTRY_USERNAME', passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]) {
-                    // Authenticate with Docker Hub to push Docker image
+                   
                     sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
                     
-                    // Build Docker image for app.py
                     sh "docker build -t ${imageNameapp} ."
 
                     sh "docker tag ${imageNameapp} docker.io/${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${imageTagApp}"
                     
-                    // Push the app Docker image to Docker Hub
                     sh "docker push docker.io/${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${imageTagApp}"
                     
-                    // Remove the locally built app Docker image
                     sh "docker rmi -f ${imageNameapp}"
                 }
             }
@@ -35,18 +45,15 @@ pipeline {
         stage('Deploy to OpenShift') {
             steps {
                 script {
-                    withCredentials([file(credentialsId: 'OCEE', variable: 'KUBECONFIG_FILE')]) {
-                    sh "export KUBECONFIG=\$KUBECONFIG_FILE"
-                        
-                    // Replace the placeholder with the actual Docker image in the Kubernetes YAML files
-                    sh "sed -i \'s|image:.*|image: ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${imageTagApp}|g\' ./deployment.yml"
-                    
-                    // Apply the deployment file
-                    sh "oc apply -f deployment.yml -n ${OPENSHIFT_PROJECT}"
-                
+                    withCredentials([string(credentialsId: 'Openshift2', variable: 'OPENSHIFT_SECRET')]) {
+                    sh "oc login --token=\${OPENSHIFT_SECRET} \${OPENSHIFT_SERVER} --insecure-skip-tls-verify"
+                    }
+                    sh "oc project \${OPENSHIFT_PROJECT}"
+                    sh "oc new-app \${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${BUILD_NUMBER}"
+                    sh "oc apply -f deployment.yml "
+                    sh "oc create route edge --service \${APP_SERVICE_NAME} --port \${APP_PORT} --hostname \${APP_HOST_NAME} --insecure-policy Redirect"
                 }
             }
         }
-    }
 }
 }
