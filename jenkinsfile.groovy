bdb35df8376ea3a0155925cc3125c05d1b6ee838
EzzatELshazly <65233473+EzzatELshazly@users.noreply.github.com>
2024-01-30 15:21:16 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,15 +4,16 @@ pipeline {
     environment {
         
         DOCKER_REGISTRY = "ezzatelshazly"
-        DOCKER_IMAGE = "firstpipeline"
+        DOCKER_IMAGE = "task6-jenkins-pipeline-1"
         imageTagApp = "build-${BUILD_NUMBER}-app"
         imageNameapp = "${DOCKER_REGISTRY}:${imageTagApp}"
         OPENSHIFT_PROJECT = 'ezzatelshazly'
         GITHUB_REPO = "EzzatELshazly/new-app"
         OPENSHIFT_SERVER = 'https://api.ocpuat.devopsconsulting.org:6443'
-        APP_SERVICE_NAME = 'servicePipelineEE'
+        APP_SERVICE_NAME = 'task6-jenkins-pipeline-1'
         APP_PORT = '8080'
-        APP_HOST_NAME = 'servicePipelineEE.apps.ocpuat.devopsconsulting.org'
+        APP_HOST_NAME = 'task6-jenkins-pipeline-1.apps.ocpuat.devopsconsulting.org'
+        
         }
 
     stages {
@@ -24,16 +25,30 @@ pipeline {
             }
         }
         
-        stage('Build Docker image for app.py and push it to docker hub') {
+        stage('Unit Test before building image') {
             steps {
-                withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_REGISTRY_USERNAME', passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]) {
-                   
-                    sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
-                    
+                sh "chmod +x gradlew"
+                sh "./gradlew test --stacktrace"
+            }
+        }
+        
+        
+        stage('Build Docker image') {
+            steps {
+
                     sh "docker build -t ${imageNameapp} ."
 
                     sh "docker tag ${imageNameapp} docker.io/${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${imageTagApp}"
                     
+                }
+            }
+        
+        stage('Push image to docker hub') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_REGISTRY_USERNAME', passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]) {
+                   
+                    sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
+
                     sh "docker push docker.io/${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${imageTagApp}"
                     
                     sh "docker rmi -f ${imageNameapp}"
@@ -41,19 +56,25 @@ pipeline {
             }
         }
 
-
-        stage('Deploy to OpenShift') {
+        stage('Deploy image to OpenShift') {
             steps {
                 script {
-                    withCredentials([string(credentialsId: 'Openshift2', variable: 'OPENSHIFT_SECRET')]) {
+                    withCredentials([string(credentialsId: 'openshift-token', variable: 'OPENSHIFT_SECRET')]) {
                     sh "oc login --token=\${OPENSHIFT_SECRET} \${OPENSHIFT_SERVER} --insecure-skip-tls-verify"
                     }
+                    // To go into my project 
                     sh "oc project \${OPENSHIFT_PROJECT}"
-                    sh "oc new-app \${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${BUILD_NUMBER}"
-                    sh "oc apply -f deployment.yml "
-                    sh "oc create route edge --service \${APP_SERVICE_NAME} --port \${APP_PORT} --hostname \${APP_HOST_NAME} --insecure-policy Redirect"
+                    // delete the project within the same name to avoid conflicts
+                    sh "oc delete dc,svc,deploy,ingress,route \${DOCKER_IMAGE} || true"
+                    // new-app to create deployment, service and route. 
+                    sh "oc new-app ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${imageTagApp} --token=\${OPENSHIFT_SECRET}"
+                    // Expose the service 
+                    sh "oc expose service/${APP_SERVICE_NAME}"
+                  
                 }
             }
         }
+        
+        
 }
 }
